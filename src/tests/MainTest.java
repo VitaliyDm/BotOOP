dee81e013e26ec61c8e46ab69e5bb697e5528380
hellinsideyou <hellinsideyou@github.com>
2018-10-07 18:49:17 +0500

    лолоо
    с

--- src/tests/MainTest.java
+++ src/tests/MainTest.java
@@ -1,10 +1,10 @@
-import org.junit.Test;
+//import org.junit.Test;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
-public class MainTest {
-    @Test
-    public void multipleTest1(){
-        assertEquals(4,4);
-    }
-}
+//public class MainTest {
+//    @Test
+//    public void multipleTest1(){
+//        assertEquals(4,4);
+//    }
+//}
