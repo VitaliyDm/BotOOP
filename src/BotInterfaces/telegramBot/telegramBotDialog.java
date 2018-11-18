bada0c9d8f318eda23f2bc47891d493919663856
VovaGul <vovagul1999@mail.ru>
2018-11-18 17:49:24 +0500

    Этот работает, но нет тестов

---
+++ src/BotInterfaces/telegramBot/telegramBotDialog.java
@@ -0,0 +1,88 @@
+package BotInterfaces.telegramBot;
+
+import BotInterfaces.Dialog;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import questions.QuestionHelper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+
+public class telegramBotDialog extends Dialog {
+    private BufferedReader reader;
+    private telegramBotDialog.Bot telegramApi;
+    private ArrayDeque<String> messagesQueue = new ArrayDeque<>();
+    private long chatId;
+
+    public telegramBotDialog(QuestionHelper helper) {
+        super(helper);
+        questionHelper = helper;
+
+        ApiContextInitializer.init();
+        TelegramBotsApi api = new TelegramBotsApi();
+        telegramApi = new telegramBotDialog.Bot();
+        try {
+            api.registerBot(telegramApi);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String read() throws IOException{
+        while(true){
+            if (!messagesQueue.isEmpty()) return messagesQueue.pop();
+            try {
+                Thread.sleep(25);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void write(String text) throws IOException {
+        if (chatId != 0){
+            telegramApi.sendMessage(text);
+        }
+    }
+
+    class Bot extends TelegramLongPollingBot {
+        void sendMessage(String text){
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.enableMarkdown(true);
+            message.setText(text);
+            try {
+                sendMessage(message);
+            } catch (TelegramApiException e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onUpdateReceived(Update update) {
+            Message message = update.getMessage();
+            chatId = message.getChatId();
+            if (message.hasText()){
+                messagesQueue.add(message.getText());
+            }
+        }
+
+        @Override
+        public String getBotUsername() {
+            return "WhoWhereWhenBot";
+        }
+
+        @Override
+        public String getBotToken() {
+            return "697301961:AAEdrNBIeEN_hIMi07qmqd4La-rfPwmC4go";
+        }
+    }
+}
