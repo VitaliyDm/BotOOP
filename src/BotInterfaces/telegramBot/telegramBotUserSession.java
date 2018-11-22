92d7d3dc366738c569cb705bc0e59cf3f02d61d2
VovaGul <vovagul1999@mail.ru>
2018-11-22 23:08:31 +0500

    работает для 2 игроков и более(возможно)

--- src/BotInterfaces/telegramBot/telegramBotUserSession.java
+++ src/BotInterfaces/telegramBot/telegramBotUserSession.java
@@ -1,6 +1,5 @@
 package BotInterfaces.telegramBot;
 
-import BotInterfaces.ConsoleBot.ConsoleBotDialog;
 import BotInterfaces.UserSession;
 import questions.QuestionsGenerator;
 
@@ -9,13 +8,37 @@ import java.io.IOException;
 public class telegramBotUserSession extends UserSession {
     public telegramBotUserSession(QuestionsGenerator questionsGenerator) throws IOException {
         super(questionsGenerator);
-
         userDialog = new telegramBotDialog(questionHelper);
         startDialog();
     }
 
     @Override
     public void startDialog() throws IOException {
-        userDialog.mainDialog();
+        Thread myThready = new Thread(new Runnable()
+        {
+            public void run()  //Этот метод будет выполняться в побочном потоке
+            {
+                try {
+                    userDialog.mainDialog();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        myThready.start();
+    }
+
+    void set_messagesQueue(String message){
+        ((telegramBotDialog) userDialog).messagesQueue.add(message);
+    }
+
+    void set_bot(Bot bot){
+        ((telegramBotDialog) userDialog).bot = bot;
+    }
+
+    public void set_chatId(Long chatId){
+        ((telegramBotDialog) userDialog).chatId = chatId;
     }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
 import BotInterfaces.ConsoleBot.ConsoleBotUserSession;
-import BotInterfaces.telegramBot.telegramBotUserSession;
-import BotInterfaces.UserSession;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
 import questions.QuestionsGenerator;
 
 import java.io.IOException;
@@ -11,10 +6,11 @@ import java.io.IOException;
 public class Main{
     public static void main(String[] args){
         try {
-            new telegramBotUserSession(new QuestionsGenerator(Constants.PATH_TO_QUESTIONS));
+            new ConsoleBotUserSession(new QuestionsGenerator(Constants.PATH_TO_QUESTIONS));
         } catch (IOException e){
             e.printStackTrace();
         }
+
 
     }
 }
