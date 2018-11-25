556a7b13c82531dc0f41eaa4d856be7574fc01dc
VitaliyDm <vitaldmitrie@gmail.com>
2018-11-25 16:30:59 +0500

    Rewrited sql getter

--- src/mysqlWork/Getters/GetTelegramSession.java
+++ src/mysqlWork/Getters/GetTelegramSession.java
@@ -1,19 +1,22 @@
 package mysqlWork.Getters;
 
+import mysqlWork.SessionInfo;
 import mysqlWork.SqlGetter;
 
 import java.sql.SQLException;
 
 public class GetTelegramSession extends SqlGetter {
-    class 
+    private SessionInfo gettedSession = new SessionInfo();
+
     @Override
     protected void requestAction() throws SQLException {
         var chatId = resultSet.getLong("chatId");
-        var score = resultSet.getInt("score");
-        var userQuestions = resultSet.getString("userQuestions");
+        gettedSession.Score = resultSet.getInt("score");
+        gettedSession.UserQuestions = resultSet.getString("userQuestions");
     }
 
-    protected void getUserSession(Long chatId){
+    public SessionInfo getUserSession(Long chatId){
         getRequest(String.format("select * from users where chatId=%d", chatId));
+        return gettedSession;
     }
 }
