7849d59c00575c75a8735a8eb3a69925a3c5a9db
VitaliyDm <vitaldmitrie@gmail.com>
2018-11-26 12:23:44 +0500

    added import to tests

--- src/tests/TelegramSessionSetterTest.java
+++ src/tests/TelegramSessionSetterTest.java
@@ -2,9 +2,9 @@ package tests;
 
 import mysqlWork.Getters.TelegramSessionGetter;
 import mysqlWork.Setters.TelegramSessionSetter;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TelegramSessionSetterTest {
     @Test
