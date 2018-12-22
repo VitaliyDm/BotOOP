e95ba9f1c64b0e80097c1993fc183a223a7fd98e
VovaGul <vovagul1999@mail.ru>
2018-12-23 00:30:36 +0500

    xml files

--- src/constants/SqlConnectionConst.java
+++ src/constants/SqlConnectionConst.java
@@ -1,7 +1,7 @@
 package constants;
 
 public final class SqlConnectionConst {
-    public static final String url = "jdbc:mysql://localhost:3306/mydb?autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    public static final String url = "jdbc:mysql://localhost:3306/botoop?autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     public static final String user = "root";
-    public static final String password = "root";
+    public static final String password = "1234";
 }
